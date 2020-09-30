27fcc8aaadd3e1a136f3409879f7d90370f31c4a
cyn237 <72126514+cyn237@users.noreply.github.com>
2020-10-01 01:19:06 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
- #import socket module
+#import socket module
 from socket import *
 import sys # In order to terminate the program
 
@@ -6,25 +6,20 @@ def webServer(port=13331):
    serverSocket = socket(AF_INET, SOCK_STREAM)
 
    #Prepare a sever socket
-   #Fill in start
    serverSocket.bind(('', 13331))
    serverSocket.listen(1)
-   #Fill in end
-
    while True:
        #Establish the connection
        print('Ready to serve...')
-       connectionSocket, addr = serverSocket.accept()# Fill in start #Fill in end
+       connectionSocket, addr = serverSocket.accept()
        try:
-           message = connectionSocket.recv(1024).decode()# Fill in start   # Fill in end
+           message = connectionSocket.recv(1024).decode()
            filename = message.split()[1]
            f = open(filename[1:])
-           outputdata = f.read() #Fill in start     #Fill in end
+           outputdata = f.read()
 
            #Send one HTTP header line into socket
-           #Fill in start
            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-           #Fill in end
 
            #Send the content of the requested file to the client
            for i in range(0, len(outputdata)):
@@ -34,14 +29,10 @@ def webServer(port=13331):
            connectionSocket.close()
        except IOError:
            #Send response message for file not found (404)
-           #Fill in start
            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-           #Fill in end
 
            #Close client socket
-           #Fill in start
            connectionSocket.close()
-           #Fill in end
 
    serverSocket.close()
    sys.exit()  # Terminate the program after sending the corresponding data
